8614b425cb680b65db386a481d2f44ff6849f3ca
Michael Roth <RothM5@basfad.basf.net>
2025-03-07 12:03:35 +0100

    add test

--- FillingHandlerTests/MathTests.cs
+++ FillingHandlerTests/MathTests.cs
@@ -38,6 +38,31 @@ public sealed class MathTests
 
         Assert.AreEqual(DateTime.Parse("2024-03-05T12:45"), start + end!.Value);
 
-        //TODO Rücksetzer testen
+        
+    }
+
+    [TestMethod]
+    public void ScaleHelperWithOneResetTest()
+    {
+        DateTime start;
+        double delta;
+        TimeSpan? end;
+
+        start = DateTime.Parse("2024-03-05T12:00");
+        delta = 150d; // Ziel: 50 kg
+
+        //Rücksetzer testen
+        var points = new List<Point>
+        {
+            new() { Time = DateTime.Parse("2024-03-05T12:00"), Value = 0.0 }, //0kg
+            new() { Time = DateTime.Parse("2024-03-05T13:00"), Value = 100.0 }, //100kg
+            new() { Time = DateTime.Parse("2024-03-05T13:01"), Value = 0 }, //100kg
+            new() { Time = DateTime.Parse("2024-03-05T14:00"), Value = 0 }, //100kg
+            new() { Time = DateTime.Parse("2024-03-05T15:00"), Value = 100.0 }, //100kg
+        };
+
+        end = points.GetDeltaTime(delta, start);
+
+        Assert.AreEqual(DateTime.Parse("2024-03-05T14:30"), start + end!.Value);
     }
 }
